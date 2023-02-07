8eb85b9f3a114215089d807287ff9db876d94df7
hariprasaath <hariprasaath090401@gmail.com>
2023-02-07 23:10:39 +0530

    rule fetch page with springboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { RulefetchComponent } from './rulefetch/rulefetch.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './component/login/login.component';
+import { Rule } from './model/rule';
+import { MatTableModule } from '@angular/material/table';
 //import { UserComponent } from './user/user.component';
 
 @NgModule({
@@ -16,15 +18,15 @@ import { LoginComponent } from './component/login/login.component';
     RulefetchComponent,
     SignupComponent,
     LoginComponent,
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatTableModule,
   ],
-  providers: [],
+  providers: [Rule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/rule.ts
@@ -0,0 +1,65 @@
+export class Rule {
+  private _sid!: BigInteger;
+  public get sid(): BigInteger {
+    return this._sid;
+  }
+  public set sid(value: BigInteger) {
+    this._sid = value;
+  }
+  private _protocol!: string;
+  public get protocol(): string {
+    return this._protocol;
+  }
+  public set protocol(value: string) {
+    this._protocol = value;
+  }
+  private _srcip!: string;
+  public get srcip(): string {
+    return this._srcip;
+  }
+  public set srcip(value: string) {
+    this._srcip = value;
+  }
+  private _src_port!: string;
+  public get src_port(): string {
+    return this._src_port;
+  }
+  public set src_port(value: string) {
+    this._src_port = value;
+  }
+  private _dst_ip!: string;
+  public get dst_ip(): string {
+    return this._dst_ip;
+  }
+  public set dst_ip(value: string) {
+    this._dst_ip = value;
+  }
+  private _dst_port!: string;
+  public get dst_port(): string {
+    return this._dst_port;
+  }
+  public set dst_port(value: string) {
+    this._dst_port = value;
+  }
+  private _message!: string;
+  public get message(): string {
+    return this._message;
+  }
+  public set message(value: string) {
+    this._message = value;
+  }
+  private _num_pkts!: string;
+  public get num_pkts(): string {
+    return this._num_pkts;
+  }
+  public set num_pkts(value: string) {
+    this._num_pkts = value;
+  }
+  private _user!: string;
+  public get user(): string {
+    return this._user;
+  }
+  public set user(value: string) {
+    this._user = value;
+  }
+}
